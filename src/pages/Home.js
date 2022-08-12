6bde6f27fcc70a122c6e152934ec9fec32450134
juanmagal10 <juanmagalvan.24@gmail.com>
2022-08-12 19:32:56 -0300

    fotter created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Carousel from '../components/Body/Carousel'
-import { images } from '../assets'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Header from '../components/Header/Header';
+import Carousel from '../components/Body/Carousel';
+import Footer from '../components/Footer/Footer';
+import { images } from '../assets';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Carousel images={images}></Carousel>
+      <Footer></Footer>
     </div>
   )
 }
