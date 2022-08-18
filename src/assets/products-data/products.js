ca6f3b322927091f952a3207cb1e5e8e2b38f5bd
juanmagal10 <juanmagalvan.24@gmail.com>
2022-08-18 17:53:09 -0300

    merge fixed

--- src/assets/products-data/products.js
+++ src/assets/products-data/products.js
@@ -1,57 +1,71 @@
-export const productsData = [[{
-    img: require('./sony-a7-iv.jpg'),
-    name: 'Sony a 7iv',
-    description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-    url: 'sony-a7iv'
-},
- 
-    {img: require('./sony-a7-iv.jpg'),
+export const productsData = [{
+    category: 'fotografia', 
+    products:[ {
+            img: require('../sony-a7-iv.jpg'),
+            name: 'Sony a 7iv',
+            description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
+            url: 'sony-a7iv', 
+    },
+    {img: require('../sony-a7-iv.jpg'),
         name: 'black magic', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'black-magic'
+        url: 'black-magic', 
+        
     }, 
 
-    {img: require('./sony-a7-iv.jpg'),
+    {img: require('../sony-a7-iv.jpg'),
         name: 'sigma 35mm', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'sigma-35'
-    }], 
+        url: 'sigma-35', 
+    
+    }]
+},
 
-    [{
-    img: require('./sony-a7-iv.jpg'),
-    name: 'luz led azul',
-    description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-    url: 'sony-a7iv'
+    {
+        category: 'luces', 
+        products:[{
+        img: require('../sony-a7-iv.jpg'),
+        name: 'luz led azul',
+        description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
+        url: 'luz-led-azul', 
+
 },
  
-    {img: require('./sony-a7-iv.jpg'),
-        name: 'luz led blanca', 
+    {img: require('../sony-a7-iv.jpg'),
+        name: 'luz-led-azul', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'black-magic'
+        url: 'luz-led-blanca', 
     }, 
 
-    {img: require('./sony-a7-iv.jpg'),
+    {img: require('../sony-a7-iv.jpg'),
         name: 'luz led roja', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'sigma-35'
-        }],
+        url: 'luz-led-roja', 
+        }]
+    },
     
-    [{
-    img: require('./sony-a7-iv.jpg'),
+    {
+        category: 'sonido', 
+        products:[{
+    img: require('../sony-a7-iv.jpg'),
     name: 'micro estudio',
     description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-    url: 'sony-a7iv'
+    url: 'micro-estudio', 
+    category:'sonido'
 },
  
-    {img: require('./sony-a7-iv.jpg'),
+    {img: require('../sony-a7-iv.jpg'),
         name: 'micro afuera', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'black-magic'
+        url: 'micro-afuera', 
+        category:'sonido'
     }, 
 
-    {img: require('./sony-a7-iv.jpg'),
+    {img: require('../sony-a7-iv.jpg'),
         name: 'micro zapata', 
         description: 'cmkiqmoc cmqkowmckq cmpoqwmckqmwckq mckqwmcopqw mcoqpmcpqwc', 
-        url:'sigma-35'
-    }],
+        url: 'micro-zapata', 
+        category:'sonido'
+    }]
+    },
 ]
